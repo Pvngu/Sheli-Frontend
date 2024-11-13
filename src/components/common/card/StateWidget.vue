9b793e579d56b78249f026a4506d2ba50e445775
Pvngu <omargt151102@gmail.com>
2024-11-13 02:15:33 -0800

    Add StateWidget component and integrate it into the Dashboard view

---
+++ src/components/common/card/StateWidget.vue
@@ -0,0 +1,36 @@
+<template>
+    <a-card :bordered="false">
+        <a-row :gutter="16">
+            <a-space>
+                <a-col>
+                    <div class="state-widget-icon" :style="{ backgroundColor: bgColor }">
+                        <slot name="image"></slot>
+                    </div>
+                </a-col>
+                <a-col>
+                    <slot name="description"></slot>
+                </a-col>
+            </a-space>
+        </a-row>
+    </a-card>
+</template>
+
+<script>
+export default {
+    props: {
+        bgColor: {
+            default: "#5F63F2",
+        }
+    }
+}
+</script>
+
+<style scoped>
+.state-widget-icon {
+    height: 60px; 
+    border-radius: 12px; 
+    display: grid; 
+    place-content: center; 
+    aspect-ratio: 1 / 1;
+}
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,11 +4,82 @@
             <a-page-header title="Dashboard" class="p-0" />
         </template>
     </MainPageHeader>
+
+    <div class="dashboard-page-content-container">
+        <a-row :gutter="[8, 8]" class="mt-30 mb-10">
+            <a-col :xs="24" :sm="24" :md="12" :lg="6" :xl="6">
+                <DateRangePicker
+                    @dateTimeChanged="
+                        (changedDateTime) => (filters.dates = changedDateTime)
+                    "
+                />
+            </a-col>
+        </a-row>
+        <div class="mt-30 mb-10">
+            <a-row :gutter="[16, 16]">
+                <a-col :xs="24" :sm="24" :md="6" :lg="8" :xl="8">
+                    <StateWidget bgColor="#08979c">
+                        <template #image>
+                            <UserOutlined style="color: #fff; font-size: 24px" />
+                        </template>
+                        <template #description>
+                            <a-statistic :value="1128" title="Total Users" />
+                        </template>
+                    </StateWidget>
+                </a-col>
+                <a-col :xs="24" :sm="24" :md="6" :lg="8" :xl="8">
+                    <StateWidget bgColor="#379e0e">
+                        <template #image>
+                            <UserOutlined style="color: #fff; font-size: 24px" />
+                        </template>
+                        <template #description>
+                            <a-statistic :value="1128" title="Total Accidents" />
+                        </template>
+                    </StateWidget>
+                </a-col>
+                <a-col :xs="24" :sm="24" :md="6" :lg="8" :xl="8">
+                    <StateWidget bgColor="#d36b08">
+                        <template #image>
+                            <UserOutlined style="color: #fff; font-size: 24px" />
+                        </template>
+                        <template #description>
+                            <a-statistic :value="1128" title="Current Injured Members" />
+                        </template>
+                    </StateWidget>
+                </a-col>
+            </a-row>
+        </div>
+    </div>
 </template>
 
-<script setup>
+<script>
 import { ref, onMounted } from 'vue';
+import { UserOutlined } from '@ant-design/icons-vue';
 import MainPageHeader from '../components/layouts/MenuPageHeader.vue';
+import DateRangePicker from '../components/common/calendar/DateRangePicker.vue';
+import StateWidget from '../components/common/card/StateWidget.vue';
+
+export default {
+    components: {
+        UserOutlined,
+        MainPageHeader,
+        DateRangePicker,
+        StateWidget,
+    },
+    setup() {
+        const filters = ref({
+            dates: [],
+        });
+
+        onMounted(() => {
+            console.log('Dashboard page mounted');
+        });
+
+        return {
+            filters,
+        }
+    }
+}
 </script>
 
 <style>
